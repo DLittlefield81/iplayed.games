66472369605485a700fb1cf58d19b0ed218b5d51
Dennis Littlefield <Littlefield.Dennis@outlook.com>
2023-06-22 13:22:23 -0400

    updated site description, started prisma

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,7 +8,7 @@ import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata = {
   title: 'iPlayed',
-  description: 'Isn\'t it time you were rewarded for your achievements?',
+  description: "iPlayed is an innovative reservation system that not only simplifies the booking process for adventure games and interactive entertainment experiences but also adds an exciting gamification element to your journey. Earn achievement badges and level up your profile as you conquer thrilling activities like escape rooms, rage rooms, axe throwing, laser tag, and paintball. With iPlayed, you can easily reserve group-based adventures while unlocking a world of accomplishments. Immerse yourself in the thrill of the game, collect badges, and showcase your progress as you embark on unforgettable experiences. Start your gamified adventure with iPlayed today and let the achievements guide your path to victory.",
 }
 
 const font = Nunito({
